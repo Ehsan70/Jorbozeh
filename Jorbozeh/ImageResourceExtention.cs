37ae4687d8a4b3cf2f09da5ceb0f7b5d6916fae8
Shawn <a.ehsan70@gmail.com>
2020-10-19 22:05:15 -0700

    .gitignore fix

---
+++ Jorbozeh/ImageResourceExtention.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Jorbozeh
+{
+    [ContentProperty (nameof(Source))]
+    class ImageResourceExtention : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if(Source == null)
+            {
+                return null;
+            }
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtention).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
