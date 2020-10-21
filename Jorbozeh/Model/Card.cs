14ba943c9ae820ab022bcd599dd913ab51b9792f
Shawn <a.ehsan70@gmail.com>
2020-10-20 23:53:21 -0700

    Added CardDeck and CardBatch

--- Jorbozeh/Model/Card.cs
+++ Jorbozeh/Model/Card.cs
@@ -6,13 +6,17 @@ namespace Jorbozeh.Model
 {
     public class Card
     {
+        public string CardDeck { get; set; }
+        public int CardBatch { get; set; }
         public string CardTitle { get; set; }
         public string CardDesc { get; set; }
         public string CardSubDesc { get; set; }
         public string CardImage { get; set; }
 
-        public Card(string cardTitle, string cardDesc, string cardSubDesc, string cardImage)
+        public Card(string cardDeck, int cardBatch, string cardTitle, string cardDesc, string cardSubDesc, string cardImage)
         {
+            CardDeck = cardDeck;
+            CardBatch = cardBatch;
             CardTitle = cardTitle;
             CardDesc = cardDesc;
             CardSubDesc = cardSubDesc;
@@ -21,7 +25,7 @@ namespace Jorbozeh.Model
 
         public override string ToString()
         {
-            return $"Id: {1}, Title: {CardTitle}, Desc: {CardDesc}, SubDesc: {CardSubDesc}";
+            return $"Deck: {CardDeck}, Batch: {CardBatch}, Title: {CardTitle}, Desc: {CardDesc}, SubDesc: {CardSubDesc}";
         }
     }
 }
